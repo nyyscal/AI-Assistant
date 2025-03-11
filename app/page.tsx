224f61e34d179909fe38fd4688c13bcc2a20a908
Nischal <nyyscal541556@gmail.com>
2025-03-11 17:17:07 +0545

    Minor Deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter()
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>AI Assistant</h1>
-      <Button>Hi</Button>
+      <Button className="cursor-pointer" onClick={()=>router.push("/sign-in")}>Hi</Button>
     </div>
   );
 }
